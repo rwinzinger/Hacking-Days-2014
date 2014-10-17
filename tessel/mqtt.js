8fb69b81626cca5cab4dca50d9812ed73737020f
Ralph Winzinger <ralph.winzinger@senacor.com>
2014-10-17 21:07:15 +0200

    mqtt und servo

--- tessel/mqtt.js
+++ tessel/mqtt.js
@@ -1,12 +1,31 @@
 var mqtt = require('mqtt')
+var tessel = require("tessel");
 
-client = mqtt.createClient(1883, 'wir-amba.fritz.box');
+var led1 = tessel.led[0].output(0);
+var led2 = tessel.led[1].output(0);
 
-client.subscribe('light');
+client = mqtt.createClient(19709, 'm20.cloudmqtt.com', {username:"evnevuat", password:"G4yO7QTrmogs", clientId:"18773"});
+
+client.subscribe('t_temp');
+
+var servolib = require("servo-pca9685");
+var servo = servolib.use(tessel.port["A"]);
+
+var maxTemp = 40;
+var minTemp = 0;
+
+var position = 0;
 
 client.on('message', function (topic, message) {
   console.log(topic+": "+message);
-  // client.end();
+  led1.toggle();
+  led2.toggle();
+
+	position += 0.1;
+	if (position > 1) {
+		position = 0;
+	}
+	servo.move(1, position);
 });
 
 client.on('connect', function () {
@@ -14,6 +33,7 @@ client.on('connect', function () {
   // client.publish('temperature', 'Hello mqtt');
 });
 
-
-
-
+process.on("exit", function() {
+	console.log("about to exit ...")
+	client.end();
+})
