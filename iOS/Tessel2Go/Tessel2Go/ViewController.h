9ef7266d3484597dd025450f4ca42583c58acc63
Bartosz Boron <b.boron86@gmail.com>
2014-10-18 16:03:19 +0200

    iOS mqtt tessel client

---
+++ iOS/Tessel2Go/Tessel2Go/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Tessel2Go
+//
+//  Created by Bartosz Boron on 18.10.14.
+//  Copyright (c) 2014 Senacor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
+
---
+++ iOS/Tessel2Go/Tessel2Go/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tessel2Go
+//
+//  Created by Bartosz Boron on 18.10.14.
+//  Copyright (c) 2014 Senacor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
