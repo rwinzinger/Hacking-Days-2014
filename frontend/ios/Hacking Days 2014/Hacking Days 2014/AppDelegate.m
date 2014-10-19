6b80582520c6befaef712a6ba88673330109ff30
Ralph Winzinger <ralph.winzinger@senacor.com>
2014-10-19 11:04:23 +0200

    simple push test

--- frontend/ios/Hacking Days 2014/Hacking Days 2014/AppDelegate.m
+++ frontend/ios/Hacking Days 2014/Hacking Days 2014/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <AudioToolbox/AudioToolbox.h>
 
 @interface AppDelegate ()
 
@@ -17,6 +18,10 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [[UIApplication sharedApplication] registerForRemoteNotificationTypes:(UIRemoteNotificationType)(UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound)];
+    
+    // See more at: http://iosapplove.com/archive/2012/10/apns-tutorial-how-to-send-pushnotifications-to-my-app/#sthash.eAaQYapP.dpuf
+    
     return YES;
 }
 
@@ -41,5 +46,161 @@
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
+
+
+/*
+ * ------------------------------------------------------------------------------------------
+ *  BEGIN APNS CODE
+ * ------------------------------------------------------------------------------------------
+ */
+
+/**
+ * Fetch and Format Device Token and Register Important Information to Remote Server
+ */
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)devToken {
+    
+#if !TARGET_IPHONE_SIMULATOR
+    
+    // Get Bundle Info for Remote Registration (handy if you have more than one app)
+    NSString *appName = @"HackingDays2014"; // [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleDisplayName"];
+    NSString *appVersion = [[[NSBundle mainBundle] infoDictionary] objectForKey:@"CFBundleVersion"];
+    
+    // Check what Notifications the user has turned on.  We registered for all three, but they may have manually disabled some or all of them.
+    NSUInteger rntypes = [[UIApplication sharedApplication] enabledRemoteNotificationTypes];
+    
+    // Set the defaults to disabled unless we find otherwise...
+    NSString *pushBadge = @"disabled";
+    NSString *pushAlert = @"disabled";
+    NSString *pushSound = @"disabled";
+    
+    // Check what Registered Types are turned on. This is a bit tricky since if two are enabled, and one is off, it will return a number 2... not telling you which
+    // one is actually disabled. So we are literally checking to see if rnTypes matches what is turned on, instead of by number. The "tricky" part is that the
+    // single notification types will only match if they are the ONLY one enabled.  Likewise, when we are checking for a pair of notifications, it will only be
+    // true if those two notifications are on.  This is why the code is written this way
+    if(rntypes == UIRemoteNotificationTypeBadge){
+        pushBadge = @"enabled";
+    }
+    else if(rntypes == UIRemoteNotificationTypeAlert){
+        pushAlert = @"enabled";
+    }
+    else if(rntypes == UIRemoteNotificationTypeSound){
+        pushSound = @"enabled";
+    }
+    else if(rntypes == ( UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeAlert)){
+        pushBadge = @"enabled";
+        pushAlert = @"enabled";
+    }
+    else if(rntypes == ( UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound)){
+        pushBadge = @"enabled";
+        pushSound = @"enabled";
+    }
+    else if(rntypes == ( UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeSound)){
+        pushAlert = @"enabled";
+        pushSound = @"enabled";
+    }
+    else if(rntypes == ( UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeSound)){
+        pushBadge = @"enabled";
+        pushAlert = @"enabled";
+        pushSound = @"enabled";
+    }
+    
+    // Get the users Device Model, Display Name, Unique ID, Token &amp; Version Number
+    UIDevice *dev = [UIDevice currentDevice];
+    NSString *deviceUuid = @"18773_RW_Senacor";
+    NSString *deviceName = dev.name;
+    NSString *deviceModel = dev.model;
+    NSString *deviceSystemVersion = dev.systemVersion;
+    
+    // Prepare the Device Token for Registration (remove spaces and &lt; &gt;)
+    NSString *deviceToken = [[[[devToken description]
+                               stringByReplacingOccurrencesOfString:@"&lt;"withString:@""]
+                              stringByReplacingOccurrencesOfString:@"&gt;" withString:@""]
+                             stringByReplacingOccurrencesOfString: @" " withString: @""];
+    
+    // Build URL String for Registration
+    // !!! CHANGE "www.mywebsite.com" TO YOUR WEBSITE. Leave out the http://
+    NSString *host = @"www.senacor.com";
+    
+    // !!! CHANGE "/apns.php?" TO THE PATH TO WHERE apns.php IS INSTALLED
+    // !!! ( MUST START WITH / AND END WITH ? ).
+    // !!! SAMPLE: "/path/to/apns.php?"
+    NSString *urlString = [@"/pushservice/apns.php?"stringByAppendingString:@"task=register"];
+    
+    NSLog(@"devicetoken length: %i", [deviceToken length]);
+    NSLog(@"Device Token: %@", deviceToken);
+    
+    urlString = [urlString stringByAppendingString:@"&amp;appname="];
+    urlString = [urlString stringByAppendingString:appName];
+    urlString = [urlString stringByAppendingString:@"&amp;appversion="];
+    urlString = [urlString stringByAppendingString:appVersion];
+    urlString = [urlString stringByAppendingString:@"&amp;deviceuid="];
+    urlString = [urlString stringByAppendingString:deviceUuid];
+    urlString = [urlString stringByAppendingString:@"&amp;devicetoken="];
+    urlString = [urlString stringByAppendingString:deviceToken];
+    urlString = [urlString stringByAppendingString:@"&amp;devicename="];
+    urlString = [urlString stringByAppendingString:deviceName];
+    urlString = [urlString stringByAppendingString:@"&amp;devicemodel="];
+    urlString = [urlString stringByAppendingString:deviceModel];
+    urlString = [urlString stringByAppendingString:@"&amp;deviceversion="];
+    urlString = [urlString stringByAppendingString:deviceSystemVersion];
+    urlString = [urlString stringByAppendingString:@"&amp;pushbadge="];
+    urlString = [urlString stringByAppendingString:pushBadge];
+    urlString = [urlString stringByAppendingString:@"&amp;pushalert="];
+    urlString = [urlString stringByAppendingString:pushAlert];
+    urlString = [urlString stringByAppendingString:@"&amp;pushsound="];
+    urlString = [urlString stringByAppendingString:pushSound];
+    
+    // Register the Device Data
+    // !!! CHANGE "http" TO "https" IF YOU ARE USING HTTPS PROTOCOL
+    NSURL *url = [[NSURL alloc] initWithScheme:@"http" host:host path:urlString];
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:url];
+    NSData *returnData = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+    NSLog(@"Register URL: %@", url);
+    NSLog(@"Return Data: %@", returnData);
+    
+#endif
+}
+
+/**
+ * Failed to Register for Remote Notifications
+ */
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+    
+#if !TARGET_IPHONE_SIMULATOR
+    
+    NSLog(@"Error in registration. Error: %@", error);
+    
+#endif
+}
+
+/**
+ * Remote Notification Received while application was open.
+ */
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
+    
+#if !TARGET_IPHONE_SIMULATOR
+    
+    NSLog(@"remote notification: %@",[userInfo description]);
+    NSDictionary *apsInfo = [userInfo objectForKey:@"aps"];
+    
+    NSString *alert = [apsInfo objectForKey:@"alert"];
+    NSLog(@"Received Push Alert: %@", alert);
+    
+    NSString *sound = [apsInfo objectForKey:@"sound"];
+    NSLog(@"Received Push Sound: %@", sound);
+    AudioServicesPlaySystemSound(kSystemSoundID_Vibrate);
+    
+    NSString *badge = [apsInfo objectForKey:@"badge"];
+    NSLog(@"Received Push Badge: %@", badge);
+    application.applicationIconBadgeNumber = [[apsInfo objectForKey:@"badge"] integerValue];
+    
+#endif
+}
+
+/*
+ * ------------------------------------------------------------------------------------------
+ *  END APNS CODE
+ * ------------------------------------------------------------------------------------------
+ */
 
 @end
